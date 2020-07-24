58507a3f671b24a15c1e2577b50784b9dfdf7584
Raphew <raphheuhh@outlook.com>
2020-07-24 18:20:39 +0200

    Make variables name good

--- src/main/java/fr/raphew/pileface/main/Main.java
+++ src/main/java/fr/raphew/pileface/main/Main.java
@@ -11,38 +11,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String nbrPS;
-        String[] pf = new String[]{"Heads", "Tails"};
+        Scanner scanner = new Scanner(System.in);
+        int numberParticipants;
+        String[] HeadsTails = new String[]{"Heads", "Tails"};
         System.out.println("How many participants are there ?");
-        nbrPS = sc.nextLine();
-        int nbrP = Integer.valueOf(nbrPS);
-        ArrayList<String> p = new ArrayList<>();
-        for (int i = 1 ; i <= nbrP ; i++){
-            String rep;
+        numberParticipants = scanner.nextInt();
+        scanner.nextLine();
+        ArrayList<String> playerName = new ArrayList<>();
+        for (int i = 1 ; i <= numberParticipants ; i++){
+            String answer;
             System.out.println("Player " + i + ", what's your name ?");
-            rep = sc.nextLine();
-            p.add(rep);
+            answer = scanner.nextLine();
+            playerName.add(answer);
         }
-        int tour;
+        int round;
         System.out.println("Thank you all for your name ! \nNow, choose how many round of the game do you want to do ?");
-        tour = sc.nextInt();
-        sc.nextLine();
-        for (int i = 1 ; i <= tour ; i ++){
+        round = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 1 ; i <= round ; i ++){
             System.out.println("Start of round " + i);
-            for (int y = 0 ; y <= p.size()-1 ; y++){
-                String rep = "a";
-                while(!rep.equalsIgnoreCase("heads") && !rep.equalsIgnoreCase("tails")) {
-                    System.out.println(p.get(y) + ", it's your turn, you think the coin is going to make heads or tails ?");
-                    rep = sc.nextLine();
+            for (int y = 0 ; y <= playerName.size()-1 ; y++){
+                String answer = "";
+                while(!answer.equalsIgnoreCase("heads") && !answer.equalsIgnoreCase("tails")) {
+                    System.out.println(playerName.get(y) + ", it's your turn, you think the coin is going to make heads or tails ?");
+                    answer = scanner.nextLine();
                 }
 
-                int piece = new Random().nextInt(2);
-                String pieceS = pf[piece];
-                if(pieceS.equalsIgnoreCase(rep)){
-                    System.out.println("Well done " + p.get(y) + ", you win !");
+                int random = new Random().nextInt(2);
+                String coinsHT = HeadsTails[random];
+                if(coinsHT.equalsIgnoreCase(answer)){
+                    System.out.println("Well done " + playerName.get(y) + ", you win !");
                 }else{
-                    System.out.println("You lost " + p.get(y) + " !!! You have to drink !");
+                    System.out.println("You lost " + playerName.get(y) + " !!! You have to drink !");
                 }
             }
         }
